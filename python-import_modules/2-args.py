388f961cc6abac4e73eadcc1d2911ff3961ec6cc
Monique-creator <m.niyobyose@alustudent.com>
2025-10-17 08:43:20 -0700

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Print the number of and the list of its arguments."""
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    count = len(args)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
