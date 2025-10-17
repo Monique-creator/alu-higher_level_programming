2d827ae0ff94ca8aebcfc26af74360b040b54b2e
Monique-creator <m.niyobyose@alustudent.com>
2025-10-17 08:33:18 -0700

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""Print the sum of 1 + 2 using add_0.add function."""
-
 if __name__ == "__main__":
     from add_0 import add
-
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
