8186daff9c8c361ca5bdcd475f0f4c590cd4620e
Monique-creator <m.niyobyose@alustudent.com>
2025-10-19 14:02:32 -0700

    simple matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Computes the square of all integers in a 2D matrix."""
+    new_matrix = []
+    for row in matrix:
+        new_row = [x ** 2 for x in row]
+        new_matrix.append(new_row)
+    return new_matrix
