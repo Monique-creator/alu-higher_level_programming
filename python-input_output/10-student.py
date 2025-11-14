dd03d8839ad5571abdada7268751c6fde5301cb6
Monique-creator <m.niyobyose@alustudent.com>
2025-11-14 10:22:04 -0800

    student file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 10-student
+Defines a class Student with JSON serialization method
+"""
+
+
+class Student:
+    """Class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student with first_name, last_name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attributes in this list
+        are retrieved. Otherwise, all attributes are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
