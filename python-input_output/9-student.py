a36309f160d750957902d4ad3d3ec54d507fa505
Monique-creator <m.niyobyose@alustudent.com>
2025-11-14 10:18:37 -0800

    student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 9-student
+Defines a class Student with JSON serialization method
+"""
+
+
+class Student:
+    """Class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student with first_name, last_name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieve a dictionary representation of a Student instance
+        """
+        return self.__dict__
+
