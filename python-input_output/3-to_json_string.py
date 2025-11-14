2970779f9ae339a561d72b4e8834dce60b6c9731
Monique-creator <m.niyobyose@alustudent.com>
2025-11-14 09:35:10 -0800

    json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 3-to_json_string
+Defines a function that returns the JSON representation of an object
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
+
