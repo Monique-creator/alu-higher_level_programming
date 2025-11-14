a7242b059554c2d5faa82010ce8296f115e51d9a
Monique-creator <m.niyobyose@alustudent.com>
2025-11-14 09:58:15 -0800

    string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string
+Defines a function that returns an object represented by a JSON string
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Return an object (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
+
