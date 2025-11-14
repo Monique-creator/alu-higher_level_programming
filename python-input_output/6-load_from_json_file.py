d5af903954acfca0a8f75a7fe6b8715d39a322fb
Monique-creator <m.niyobyose@alustudent.com>
2025-11-14 10:05:01 -0800

    file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 6-load_from_json_file
+Defines a function that creates an object from a JSON file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Create an object from a JSON file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+
