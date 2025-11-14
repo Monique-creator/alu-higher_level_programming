c81b303ae8e86912f4a4a24478b7143722ca1ffc
Monique-creator <m.niyobyose@alustudent.com>
2025-11-14 01:23:20 -0800

    square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
-Module 11-square
-Defines class Square with __str__ method
+Module 8-rectangle
+Defines class Rectangle that inherits from BaseGeometry
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
 
-class Square(Rectangle):
-    """Square class"""
+class BaseGeometry:
+    """BaseGeometry class"""
 
-    def __init__(self, size):
-        """Initialize Square with validated size"""
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+    def area(self):
+        """Raise exception since area is not implemented"""
+        raise Exception("area() is not implemented")
 
-    def __str__(self):
-        """Return square description"""
-        return f"[Square] {self.__size}/{self.__size}"
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle with validated width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
